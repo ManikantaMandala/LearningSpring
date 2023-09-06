03581392700cc427b19ed366318776da5f3e1b46
mandalavijaysurya <mandala.vijay.surya@gmail.com>
2023-09-06 22:45:07 +0530

    Rectified errors

--- src/main/java/com/mandala/spring/SpringDemo/DrawingApplication.java
+++ src/main/java/com/mandala/spring/SpringDemo/DrawingApplication.java
@@ -12,7 +12,6 @@ public class DrawingApplication {
 //        Using ApplicationContext
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
         Triangle triangle = (Triangle) applicationContext.getBean("triangle");
-
         triangle.draw();
     }
 }
