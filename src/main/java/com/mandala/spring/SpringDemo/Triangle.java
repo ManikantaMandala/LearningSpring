fbb2a7ce8e4fd94e3fe508db9b304e165c973f82
ManikantaMandal <mandala.manikanta.20cse@bmu.edu.in>
2023-09-08 16:29:24 +0530

    In this commit: I learn't about configuring a list of beans which is a bean.

--- src/main/java/com/mandala/spring/SpringDemo/Triangle.java
+++ src/main/java/com/mandala/spring/SpringDemo/Triangle.java
@@ -1,34 +1,19 @@
 package com.mandala.spring.SpringDemo;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Triangle {
-    Point pointA, pointB, pointC;
+    List<Point> points;
     public void draw(){
-        System.out.println("Point A: x = " + getPointA().getX() + " y = " + getPointA().getY());
-        System.out.println("Point B: x = " + getPointB().getX() + " y = " + getPointB().getY());
-        System.out.println("Point C: x = " + getPointC().getX() + " y = " + getPointC().getY());
+        points.forEach((p)-> System.out.println("Point = (" + p.getX() + "," + p.getY() + ")"));
     }
 
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 }
