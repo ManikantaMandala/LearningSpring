11ed333023cea3024189c6e1964f42395848f3ce
ManikantaMandal <mandala.manikanta.20cse@bmu.edu.in>
2023-09-08 16:41:16 +0530

    I lean't about autowire property: byName -> It will do dependency injection using the name of variables and id value of the beans which are configured to those variables. byType -> It will do dependency injection using the type. The important thing to remember is that there should be only 1 type of a particular class. It will set the vales by set methods defined in the class. constructor -> It will do same as byType but via contructor instantiation.

--- src/main/java/com/mandala/spring/SpringDemo/Triangle.java
+++ src/main/java/com/mandala/spring/SpringDemo/Triangle.java
@@ -4,16 +4,35 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class Triangle {
-    List<Point> points;
+    Point pointA, pointB, pointC;
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
+
     public void draw(){
-        points.forEach((p)-> System.out.println("Point = (" + p.getX() + "," + p.getY() + ")"));
+        System.out.println("Point A: x = " + getPointA().getX() + " y = " + getPointA().getY());
+        System.out.println("Point B: x = " + getPointB().getX() + " y = " + getPointB().getY());
+        System.out.println("Point C: x = " + getPointC().getX() + " y = " + getPointC().getY());
     }
 
-    public List<Point> getPoints() {
-        return points;
-    }
-
-    public void setPoints(List<Point> points) {
-        this.points = points;
-    }
 }
