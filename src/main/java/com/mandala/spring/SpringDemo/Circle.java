d28887446d2b0bda0c0673b9da2bc3f7fbddba78
ManikantaMandal <mandala.manikanta.20cse@bmu.edu.in>
2023-09-17 17:01:35 +0530

    In this commit, It shows about the annotation @Component using .xml file

--- src/main/java/com/mandala/spring/SpringDemo/Circle.java
+++ src/main/java/com/mandala/spring/SpringDemo/Circle.java
@@ -7,11 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class Circle implements Shape{
 
-//    @Autowired
-//    @Qualifier("origin")
+    @Autowired
+    @Qualifier("origin")
     private Point origin;
 
     @Override
